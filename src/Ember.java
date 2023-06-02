91bed78388db53ec7e0d4a08e11cdcb00ca0b3df
Armim8jgc6 <bed.armand@yahoo.com>
2023-06-02 19:52:00 +0200

    Először létrehoztam az Ember nevű osztályt, amely az alkalmazás alap modellje.

---
+++ src/Ember.java
@@ -0,0 +1,17 @@
+public class Ember {
+    private String nev;
+    private int kor;
+
+    public Ember(String nev, int kor) {
+        this.nev = nev;
+        this.kor = kor;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public int getKor() {
+        return kor;
+    }
+}
