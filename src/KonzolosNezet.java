2c388ef1bc916e0bcd9b948235daeb6c8cee0ada
Armim8jgc6 <bed.armand@yahoo.com>
2023-06-02 19:54:50 +0200

    Majd a feladat kiírásban szereplő konzolos megjelenítéshez elkészítettem a modell kiíratását.

---
+++ src/KonzolosNezet.java
@@ -0,0 +1,7 @@
+public class KonzolosNezet {
+    public void megjelenitEmber(Ember ember) {
+        System.out.println("Név: " + ember.getNev());
+        System.out.println("Kor: " + ember.getKor());
+        System.out.println("----------------------------");
+    }
+}
